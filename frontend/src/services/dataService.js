34a5fd4870eca0358a8a0ca5525901e4e25da1b3
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-04-26 14:26:27 -0700

    Add a .env file to connect to the API

--- frontend/src/services/dataService.js
+++ frontend/src/services/dataService.js
@@ -1,24 +1,26 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 class DataService {
 
     async getProducts() {
-        let response = await axios.get("http://127.0.0.1:5000/api/catalog")
+        let response = await axios.get(`${API_URL}/api/catalog`);
         return response.data;
     }
 
     async saveProduct(product) {
-        let response = await axios.post("http://127.0.0.1:5000/api/catalog", product);
+        let response = await axios.post(`${API_URL}/api/catalog`, product);
         return response.data;
     }
 
     async getCoupons() {
-        let response = await axios.get("http://127.0.0.1:5000/api/coupons")
+        let response = await axios.get(`${API_URL}/api/coupons`);
         return response.data;
     }
 
     async saveCoupon(coupon) {
-        let response = await axios.post("http://127.0.0.1:5000/api/coupons", coupon)
+        let response = await axios.post(`${API_URL}/api/coupons`, coupon);
         return response.data;
     }
 }
